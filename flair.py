fb7fd59468ffca8f22693fadef29f7973d50ca5b
Jonathan Silberberg <yonijonathan@gmail.com>
2020-09-17 22:59:06 -0400

    fix missing variable assignment

--- flair.py
+++ flair.py
@@ -65,6 +65,8 @@ def increment_flair_text(flair_text):
 def get_css_class(flair_text):
     if type(flair_text) != int:
         i = get_value_from_flair(flair_text)
+    else:
+        i = flair_text
     
     if 1 <= i < 5:
         return 'bookbrown'
